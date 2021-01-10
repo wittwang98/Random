9ee4252a6b223e0c8452f4387bfea6713db2959f
wittwang98 <58908440+wittwang98@users.noreply.github.com>
2021-01-10 04:12:17 +0100

    Update Pi_approximation.py

--- Pi_approximation.py
+++ Pi_approximation.py
@@ -12,8 +12,8 @@ for a in range(points):
     b = (random.random(), random.random())
     if (b[0]**2 + b[1]**2)**(0.5) <= 1:
         inside += 1
-    if a%(points/100) == False:
+""" if a%(points/100) == False:
         percentage += 1
-        print(percentage, "%")
+        print(percentage, "%")"""
 end_time = time()-start    
 print("Pi is approximatively ", (4.0*inside/points), " The calculation took ", end_time, " seconds")
